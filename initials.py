16a3c5567d48322d5ab07ee94277fdd7e01f8d7d
Jesseman365 <jesseobeng36@gmail.com>
2026-05-31 22:30:32 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,6 +1,31 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Jessey Obeng Akonnor
+# STUDENT ID:  2425400854
+# Email: jesseobeng36@gmail.com
+# Grader: Augustus
+#
+# On my honor, Jessey, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+# this function prints out my initials in large letters.
+    print()
+    print("...JOA")
+    print()
+    print("...JJJJJJJJJJJJ........OOOOOOOOOOOO.............AAA.......")
+    print("............JJJ........000......000...........AA...AA.....")
+    print("............JJJ........000......000...........AA....AA....")
+    print("............JJJ........000......000...........AA.....AA...")
+    print("............JJJ........000......000...........AA.....AA...")
+    print("............JJJ........000......000...........AAAAAAAAA...")
+    print("...JJJ......JJJ........000......000...........AA.....AA...")
+    print("...JJJ......JJJ........000......000...........AA.....AA...")
+    print("...JJJJJJJJJJJJ...**....00......00....**......AA.....AA...")
+    print("....JJJJJJJJJ.....**....0000000000....**......AA.....AA...")
+    print()
 
 
 
